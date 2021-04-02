907a3781234daf0e2b4b88cd92ddfe7e4c204d3f
vityokklymets <vityokklymets@gmail.com>
2021-04-02 16:15:13 +0300

    btns listeners

--- public/tmp/js/main.js
+++ public/tmp/js/main.js
@@ -1,5 +1,14 @@
-function perfect(btn){
-    var elem = document.querySelector("#perfect-form");
-    elem.classList.toggle('show');
-}
+window.addEventListener('load',()=>{
+    const btnClickHandler = (formId) => {
+        const form = document.querySelector(formId);
+        form.classList.toggle('show');
+    }
 
+    const btns = document.querySelectorAll('.form-btn')
+
+    btns.forEach(btn=>{
+        btn.addEventListener('click',()=>{
+            btnClickHandler('#perfect-form')
+        })
+    })
+})
